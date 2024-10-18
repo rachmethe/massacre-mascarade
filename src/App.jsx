1f37ece9bc510181ab297573c49835465a596588
rachmethe <ugandasabo@gmail.com>
2024-10-18 11:03:29 +0300

    Added header and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ function App() {
 
   return (
     <>
-      <img src={logo}/>
+      <header className='flex justify-between items-center text-black py-6 px-8 md:px-32 bg-white drop-shadow-md'>
+        <a href='#'>
+          <img src={logo} alt='Massacre-Mascarade-Logo' className='w-52 hover:scale-105 transition-all'/>
+        </a>
+
+        <ul className='hidden xl:flex items-center gap-12 font-semibold text-base'>
+          <li className='p-3 hover:bg-red-800 hover:text-white rounded-md transition-all cursor-pointer font-bold'>ПРОДУКТЫ</li>
+          <li className='p-3 hover:bg-red-800 hover:text-white rounded-md transition-all cursor-pointer font-bold'>КОНТАКТЫ</li>
+        </ul>
+      </header>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>    
