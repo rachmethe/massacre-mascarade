0e63b2c4281df874fe83f2ab3a62af8f8d7c2e1e
rachmethe <ugandasabo@gmail.com>
2024-10-20 06:24:14 +0300

    test new design

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,34 @@ function App() {
 
   return (
     <>
-      <header className='flex justify-between items-center text-black py-6 px-8 md:px-32 bg-white drop-shadow-md'>
-        <a href='#'>
-          <img src={logo} alt='Massacre-Mascarade-Logo' className='w-52 hover:scale-105 transition-all'/>
-        </a>
+    <body className='bg-cover bg-center bg-[url("https://raw.githubusercontent.com/rachmethe/massacre-mascarade/refs/heads/main/public/images/bg-img-sm.jpg")]'>
+    
+        <header className='bg-transparent flex justify-between items-center text-[#A75155] py-6 px-8 md:px-32 drop-shadow-md'>
+            <a href='#'>
+            <img src={logo} alt='Massacre-Mascarade-Logo' className='w-96 hover:scale-105 transition-all'/>
+            </a>
 
-        <ul className='hidden xl:flex items-center gap-12 font-semibold text-base'>
-          <li className='p-3 hover:bg-red-800 hover:text-white rounded-md transition-all cursor-pointer font-black'>ПРОДУКТЫ</li>
-          <li className='p-3 hover:bg-red-800 hover:text-white rounded-md transition-all cursor-pointer font-black'>КОНТАКТЫ</li>
-        </ul>
-      </header>
-      <section className="py-24">
+            <ul className='hidden xl:flex items-center gap-12 font-semibold text-base'>
+            <li className='p-3 hover:bg-red-900 hover:text-white rounded-md transition-all cursor-pointer font-black'>ПРОДУКТЫ</li>
+            <li className='p-3 hover:bg-red-900 hover:text-white rounded-md transition-all cursor-pointer font-black'>КОНТАКТЫ</li>
+            </ul>
+        </header>
+        <section className="py-24">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <h2 className="font-bold text-4xl text-black mb-8 max-lg:text-center">
+            <h2 className="font-bold text-4xl text-[#A75155] mb-8 max-lg:text-center">
                 ПРОДУКТЫ
             </h2>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 bg-[#34090B] px-10 py-10 rounded-2xl">
                 <a href="javascript:;"
-                    className="mx-auto sm:mr-0 group cursor-pointer lg:mx-auto bg-white transition-all duration-500">
-                    <div className="">
+                    className="mx-auto sm:mr-0 group cursor-pointer lg:mx-auto bg-[#34090B] transition-all duration-500">
+                    <div >
                         <img src="https://github.com/rachmethe/massacre-mascarade/blob/main/public/images/vert-green.jpg?raw=true" alt="face cream image"
                             className="w-full aspect-auto rounded-2xl object-cover" />
                     </div>
                     <div className="mt-5">
-                        <div className="flex items-center justify-between">
+                        <div className="flex items-center justify-between bg-slate-300">
                             <h6
-                                className="font-semibold text-xl leading-8 text-black transition-all duration-500 group-hover:text-red-900 hover:underline">
+                                className="font-semibold text-xl leading-8 text-black transition-all duration-500 group-hover:text-red-900 hover:underline rounded-2xl">
                                 КОРОТКИЙ ЗЕЛЁНЫЙ ПОВОДОК</h6>
                             <h6 className="font-semibold text-xl leading-8 text-red-900">&#8381;10000</h6>
                         </div>
@@ -56,8 +58,8 @@ function App() {
                 </a>               
             </div>
         </div>
-    </section>
-                                             
+        </section>
+    </body>
     </>
   )
 }
